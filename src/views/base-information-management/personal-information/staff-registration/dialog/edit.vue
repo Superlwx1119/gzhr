fb7661dbb8c365e00f107032493b375ecbc56e6f
CN32333 <weixiang.liu@powersi.com>
2020-07-07 09:35:54 +0800

    更新

--- src/views/base-information-management/personal-information/staff-registration/dialog/edit.vue
+++ src/views/base-information-management/personal-information/staff-registration/dialog/edit.vue
@@ -38,10 +38,7 @@
             <EmploymentContract />
           </el-tab-pane>
         </el-tabs>
-        <div class="box-header handle">
-          <span class="box-title">业务材料</span>
-        </div>
-        <List :is-border="true" :is-index="true" :columns="columns" :operates="operation === 'detail'||operation === 'apply'?tableOperatesEdit:tableOperates" :data="tableData" />
+
       </div>
     </div>
 
@@ -109,31 +106,8 @@ export default {
       activeName: '1',
       queryForm: {},
       isShowAdd: false,
-      loading: false,
-      tableOperates: {
-        width: '120px',
-        fixed: 'right',
-        list: [
-          { show: true, label: '上传', type: 'text', method: () => {
-          } }
-        ]
-      },
-      tableOperatesEdit: {
-        width: '120px',
-        fixed: 'right',
-        list: []
-      },
-      columns: [
-        { label: '附件数', prop: '附件数', align: 'center' },
-        { label: '材料名称', prop: '材料名称', align: 'center' },
-        { label: '材料说明', prop: '材料说明', align: 'center', width: '300px' },
-        { label: '是否必须', prop: '是否必须', align: 'center' }
-      ],
-      tableData: [
-        { 单位编码: '单位编码', 单位名称: '单位名称', 社会信用代码: '社会信用代码', 单位类型: '单位类型', 是否参公: '是否参公', 主管部门: '主管部门', 行业代码: '行业代码' },
-        { 单位编码: '单位编码', 单位名称: '单位名称', 社会信用代码: '社会信用代码', 单位类型: '单位类型', 是否参公: '是否参公', 主管部门: '主管部门', 行业代码: '行业代码' },
-        { 单位编码: '单位编码', 单位名称: '单位名称', 社会信用代码: '社会信用代码', 单位类型: '单位类型', 是否参公: '是否参公', 主管部门: '主管部门', 行业代码: '行业代码' }
-      ]
+      loading: false
+
     }
   },
   methods: {
